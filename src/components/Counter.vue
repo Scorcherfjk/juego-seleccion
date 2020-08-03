cb182a290f1b4493fdf61d41e5f77e1c6b431546
Fjavier De Freitas <franjavikale@gmail.com>
2020-08-03 18:34:23 -0500

    points counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="mb-4">
+    <p
+      class="px-2 py-1 w-3/6 mx-auto rounded-full
+      text-center text-white bg-green-500"
+    >
+      200 opciones <i class="fa fa-paw"></i>
+    </p>
+  </div>
+</template>
