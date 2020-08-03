875e74d4c7288aff2b9cc5ddfec3cc6dd1e43eaf
Fjavier De Freitas <franjavikale@gmail.com>
2020-08-03 18:55:52 -0500

    cambio en las opciones

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-1/3 m-4">
-    <div class="h-48 w-32 mx-auto bg-blue-700 bg-opacity-75"></div>
+  <div class="w-1/2 my-4">
+    <div class="h-32 w-3/4 mx-auto bg-blue-700 bg-opacity-75"></div>
   </div>
 </template>
