03173e1b0c95907940dd4e10249616a6b590451d
Fjavier De Freitas <franjavikale@gmail.com>
2020-08-03 18:34:38 -0500

    card

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="absolute h-64 w-11/12 mt-4 bg-black bg-opacity-75 rounded-lg">
+    <div
+      class="relative -m-3 w-2/3 p-1 mx-auto
+      rounded-md uppercase text-center bg-white"
+    >
+      {{ msg }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String
+  }
+};
+</script>
