3028de348eeaf6e9622fdd63b269d3b43544aa11
Fjavier De Freitas <franjavikale@gmail.com>
2020-08-03 18:34:47 -0500

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Nav />
+    <Counter />
+  </div>
+</template>
+
+<script>
+import Nav from './Nav';
+import Counter from './Counter';
+export default {
+  components: {
+    Nav,
+    Counter
+  }
+};
+</script>
