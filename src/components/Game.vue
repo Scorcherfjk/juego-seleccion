c1953bc769161485bc155cdb96269daa9fd04452
Fjavier De Freitas <franjavikale@gmail.com>
2020-08-03 18:46:35 -0500

    game

---
+++ src/components/Game.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-full flex flex-wrap justify-center">
+    <Options />
+    <Options />
+    <Options />
+  </div>
+</template>
+
+<script>
+import Options from './Options';
+
+export default {
+  components: {
+    Options
+  }
+};
+</script>
