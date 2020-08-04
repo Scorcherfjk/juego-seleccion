cf3868448034b90ec88dee548dd95043a8790cbd
Fjavier De Freitas <franjavikale@gmail.com>
2020-08-04 16:58:37 -0500

    cambiando componente de opcion

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="game__option">
-    <div class="game__option__content"></div>
+    <button class="game__option__content rounded-lg" @click="addPoint">
+      <i class="fa fa-question text-6xl text-white" aria-hidden="true"></i>
+    </button>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    value: Number
+  },
+  methods: {
+    addPoint() {
+      this.$store.dispatch('addPoints', this.$props.value);
+    }
+  }
+};
+</script>
