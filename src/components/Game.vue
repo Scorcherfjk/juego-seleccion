296281a0fef4bd1f7745afcef1de27493ad3f66c
Fjavier De Freitas <franjavikale@gmail.com>
2020-08-04 16:58:05 -0500

    opviones variables

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="game">
-    <Options />
-    <Options />
-    <Options />
+    <Options v-for="(val, index) in shuffleNumbers" :value="val" :key="index" />
   </div>
 </template>
 
@@ -12,6 +10,32 @@ import Options from './Options';
 export default {
   components: {
     Options
+  },
+  data() {
+    return {
+      points: [0, 30, 12, 14]
+    };
+  },
+  computed: {
+    shuffleNumbers: function() {
+      const array = this.points;
+      let currentIndex = array.length,
+        temporaryValue,
+        randomIndex;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+      return array;
+    }
   }
 };
 </script>
