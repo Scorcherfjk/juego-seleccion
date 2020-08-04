3527b963ca80078ef9d845587e8e6be611365d69
Fjavier De Freitas <franjavikale@gmail.com>
2020-08-04 16:58:49 -0500

    agregando estado global

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="counter">
     <p class="counter__title">
-      200 opciones
+      {{ $store.state.points }} opciones
       <i class="fa fa-paw"></i>
     </p>
   </div>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,20 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    points: 0
+  },
+  mutations: {
+    incrementPoints(state, n) {
+      state.points += n;
+    }
+  },
+  actions: {
+    addPoints({ commit }, payload) {
+      commit('incrementPoints', payload);
+    }
+  }
 });
