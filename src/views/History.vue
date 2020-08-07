ab08c1f2e46ef2f83c2dd0088c9cf3d4a38fecb7
Fjavier De Freitas <franjavikale@gmail.com>
2020-08-06 20:45:24 -0500

    reduccion del codigo en las vistas

--- src/views/History.vue
+++ src/views/History.vue
@@ -3,31 +3,24 @@
   <div class="history">
     <Card msg="historial">
       <div class="card__children">
-        <div class="card__children__group">
-          <i class="card__children__group__icon fa fa-paw text-yellow-300"></i>
-          <p class="card__children__group__title ">Juega: escoge tu opción</p>
-          <p class="card__children__group__points ">
-            {{ $store.state.points }} opciones
-          </p>
-        </div>
-        <div class="card__children__group">
-          <i
-            class="card__children__group__icon fa fa-whatsapp text-green-500"
-          ></i>
-          <p class="card__children__group__title ">Comparte en Whatsapp</p>
-          <p class="card__children__group__points ">
-            1500 opciones
-          </p>
-        </div>
-        <div class="card__children__group">
-          <i
-            class="card__children__group__icon fa fa-facebook-official text-blue-500"
-          ></i>
-          <p class="card__children__group__title">Comparte en Facebook</p>
-          <p class="card__children__group__points">
-            250000 opciones
-          </p>
-        </div>
+        <InfoBox
+          icon="fa-paw"
+          colorIcon="text-yellow-300"
+          message="Juega: escoge tu opción"
+          :points="$store.state.points"
+        />
+        <InfoBox
+          icon="fa-whatsapp"
+          colorIcon="text-green-500"
+          message="Comparte en Whatsapp"
+          :points="1500"
+        />
+        <InfoBox
+          icon="fa-facebook-official"
+          colorIcon="text-blue-500"
+          message="Comparte en Facebook"
+          :points="250000"
+        />
       </div>
     </Card>
   </div>
@@ -35,10 +28,12 @@
 
 <script>
 import Card from '../components/Card';
+import InfoBox from '../components/InfoBox';
 
 export default {
   components: {
-    Card
+    Card,
+    InfoBox
   }
 };
 </script>
--- src/views/Options.vue
+++ src/views/Options.vue
@@ -3,97 +3,28 @@
   <div class="options">
     <Card msg="¡gana más opciones!">
       <div class="card__children">
-        <div class="mb-4 text-white text-center">
-          <p class="text-sm mb-2">
-            Mientras más
-            <span class="text-yellow-500 font-bold">Opciones</span>, más
-            posibilidades de ganar un viaje a
-            <span class="text-yellow-500 font-bold">Punta sal y S/ 1000</span>.
-          </p>
+        <TextField :color="'text-white'">
+          Mientras más
+          <span class="text-yellow-500 font-bold">Opciones</span>, más
+          posibilidades de ganar un viaje a
+          <span class="text-yellow-500 font-bold">Punta sal y S/ 1000</span>.
           <p class="text-xs">
             Promoción válida desde 01/01/2020 hasta 31/12/2020
           </p>
-        </div>
-        <div class="card__children__group flex-col">
-          <div class="flex w-full justify-around items-center">
-            <i
-              class="card__children__group__icon fa fa-whatsapp text-green-500 mr-2"
-            ></i>
-            <p class="card__children__group__title">
-              Comparte tu código en Whatsapp
-            </p>
-            <p
-              class="card__children__group__title text-right text-yellow-500 uppercase mr-2"
-            >
-              100 opciones por persona
-            </p>
-            <button @click="openClose">
-              <i
-                class="card__children__group__icon fa fa-chevron-down text-white"
-              ></i>
-            </button>
-          </div>
-          <div
-            class="h-48 w-full mt-4 py-2 flex flex-col items-center justify-around border-t-2"
-            v-show="open"
-          >
-            <p class="text-xs">
-              Comparte el código Z1PMzu6 con tus amigos. Cada amigo que ingrese
-              te dará 100 OPCIONES
-            </p>
-            <div
-              class="w-3/4 border px-4 py-2 rounded-full flex justify-around"
-            >
-              <span>amigos</span>
-              <span>opciones</span>
-            </div>
-            <button
-              class="w-2/4 py-1 px-4 uppercase border-2 border-green-300 text-green-300 rounded-lg"
-            >
-              compartir
-            </button>
-          </div>
-        </div>
-        <div class="card__children__group flex-col">
-          <div class="flex w-full justify-around items-center">
-            <i
-              class="card__children__group__icon fa fa-facebook-official text-blue-500 mr-2"
-            ></i>
-            <p class="card__children__group__title">
-              Comparte tu código en Facebook
-            </p>
-            <p
-              class="card__children__group__title text-right text-yellow-500 uppercase mr-2"
-            >
-              100 opciones por persona
-            </p>
-            <button @click="openClose2">
-              <i
-                class="card__children__group__icon fa fa-chevron-down text-white"
-              ></i>
-            </button>
-          </div>
-          <div
-            class="h-48 w-full mt-4 py-2 flex flex-col items-center justify-around border-t-2"
-            v-show="open2"
-          >
-            <p class="text-xs">
-              Comparte el código Z1PMzu6 con tus amigos. Cada amigo que ingrese
-              te dará 100 OPCIONES
-            </p>
-            <div
-              class="w-3/4 border px-4 py-2 rounded-full flex justify-around"
-            >
-              <span>amigos</span>
-              <span>opciones</span>
-            </div>
-            <button
-              class="w-2/4 py-1 px-4 uppercase border-2 border-green-300 text-green-300 rounded-lg"
-            >
-              compartir
-            </button>
-          </div>
-        </div>
+        </TextField>
+        <InfoBoxExpanded
+          icon="fa-whatsapp"
+          colorIcon="text-green-500"
+          userCode="xyz123"
+          message="Comparte tu código en WhatsApp"
+          :points="134"
+        />
+        <InfoBoxExpanded
+          icon="fa-facebook-official"
+          colorIcon="text-blue-500"
+          userCode="xyz123"
+          message="Comparte tu código en Facebook"
+        />
       </div>
     </Card>
   </div>
@@ -101,10 +32,14 @@
 
 <script>
 import Card from '../components/Card';
+import TextField from '../components/TextField';
+import InfoBoxExpanded from '../components/InfoBox_expanded';
 
 export default {
   components: {
-    Card
+    Card,
+    TextField,
+    InfoBoxExpanded
   },
   data() {
     return {
