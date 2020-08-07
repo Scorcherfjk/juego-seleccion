c54cd241e969b43bb773fa892fb5e50daf14eb82
Fjavier De Freitas <franjavikale@gmail.com>
2020-08-06 20:43:03 -0500

    campos de textos para los cards

---
+++ src/components/TextField.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="mb-4 text-white text-center">
+    <p :class="baseClass">
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    uppercase: Boolean,
+    color: String
+  },
+  data() {
+    let baseClass = 'text-sm mb-2';
+
+    baseClass = this.$props.uppercase ? `${baseClass} uppercase` : baseClass;
+    baseClass = this.$props.color
+      ? `${baseClass} ${this.$props.color}`
+      : baseClass;
+
+    return {
+      baseClass
+    };
+  },
+  computed: {}
+};
+</script>
