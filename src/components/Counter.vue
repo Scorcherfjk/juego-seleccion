0f3cb6cb23a44bb688565ed3cb8d1f446a9d9f39
Fjavier De Freitas <franjavikale@gmail.com>
2020-08-08 20:43:19 -0500

    estilos especificos del counter

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -6,3 +6,13 @@
     </p>
   </div>
 </template>
+
+<style lang="postcss" scoped>
+.counter {
+  @apply mb-4;
+}
+
+.counter__title {
+  @apply px-2 py-1 w-3/6 mx-auto rounded-full text-center text-white bg-green-500;
+}
+</style>
