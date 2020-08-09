d194e218845c0cf5c2c1cfa2862ba7c19c8e8c9e
Fjavier De Freitas <franjavikale@gmail.com>
2020-08-08 20:44:06 -0500

    estilos especificos de los option y stop countdown

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="game__option">
-    <button class="game__option__content rounded-lg" @click="addPoint">
-      <i class="fa fa-question text-6xl text-white" aria-hidden="true"></i>
+    <button class="game__option__content" @click="addPoint">
+      <i class="fa fa-question game__option__icon" aria-hidden="true"></i>
     </button>
   </div>
 </template>
@@ -14,7 +14,22 @@ export default {
   methods: {
     addPoint() {
       this.$store.dispatch('addPoints', this.$props.value);
+      this.$store.dispatch('startCountdown');
     }
   }
 };
 </script>
+
+<style lang="postcss" scoped>
+.game__option {
+  @apply w-1/2 my-4;
+}
+
+.game__option__content {
+  @apply block h-32 w-3/4 mx-auto bg-blue-700 bg-opacity-75 rounded-lg;
+}
+
+.game__option__icon {
+  @apply text-6xl text-white;
+}
+</style>
