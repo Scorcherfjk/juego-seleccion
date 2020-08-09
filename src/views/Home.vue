7d08a956b4d8c21f0b618337bc6525c20ef12ddc
Fjavier De Freitas <franjavikale@gmail.com>
2020-08-08 20:45:10 -0500

    add countdown to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
-    <Game />
+    <Countdown :timer="20000" v-if="$store.state.countdown" />
+    <Game v-else />
   </div>
 </template>
 
 <script>
 import Game from '../components/Game';
+import Countdown from '../components/Coundown';
 
 export default {
   components: {
-    Game
+    Game,
+    Countdown
   }
 };
 </script>
