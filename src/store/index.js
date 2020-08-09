f60e1db6a714b987af32c678abda43aba5f68c0c
Fjavier De Freitas <franjavikale@gmail.com>
2020-08-08 20:44:24 -0500

    store, stop y start countdown

--- src/store/index.js
+++ src/store/index.js
@@ -5,16 +5,29 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    points: 0
+    points: 0,
+    countdown: true
   },
   mutations: {
     incrementPoints(state, n) {
       state.points += n;
+    },
+    stopCountdown(state) {
+      state.countdown = false;
+    },
+    startCountdown(state) {
+      state.countdown = true;
     }
   },
   actions: {
     addPoints({ commit }, payload) {
       commit('incrementPoints', payload);
+    },
+    stopCountdown({ commit }) {
+      commit('stopCountdown');
+    },
+    startCountdown({ commit }) {
+      commit('startCountdown');
     }
   }
 });
