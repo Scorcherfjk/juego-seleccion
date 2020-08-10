085f3485d9dcbac828a95b89a6d11456fa4bc493
Fjavier De Freitas <franjavikale@gmail.com>
2020-08-10 12:37:08 -0500

    modal completada

--- src/components/game/Modal.vue
+++ src/components/game/Modal.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="modal">
-    <div class="flex justify-evenly w-full">
-      <button class="modal__continue" @click="continuePlaying">
+    <div class="modal__message">
+      <i class="fa fa-trophy modal__message__icon"></i>
+      <p class="modal__message__title">¡bien hecho!</p>
+      <p class="modal__message__description">
+        Has ganado
+        <span>{{ points }} opciones</span>
+      </p>
+    </div>
+    <div class="modal__buttons">
+      <button class="modal__buttons__continue" @click="continuePlaying">
         continuar
       </button>
-      <button class="modal__options">
-        más opciones
-      </button>
+      <router-link class="w-5/12" to="/options">
+        <button class="modal__buttons__options" @click="continuePlaying">
+          más opciones
+        </button>
+      </router-link>
     </div>
   </div>
 </template>
@@ -21,7 +31,6 @@ export default {
   },
   methods: {
     continuePlaying() {
-      this.$store.dispatch('addPoints', this.$props.points);
       this.$store.dispatch('startCountdown');
     }
   }
@@ -30,13 +39,38 @@ export default {
 
 <style lang="postcss" scoped>
 .modal {
-  @apply absolute flex justify-center items-end py-16 top-0 left-0 h-screen w-full bg-black opacity-50;
+  @apply absolute flex flex-col justify-between items-center py-16 top-0 left-0 h-screen w-full bg-black bg-opacity-75;
 }
-.modal__continue {
+
+.modal__message {
+  @apply flex flex-col;
+}
+
+.modal__message__icon {
+  @apply text-6xl text-center text-yellow-600;
+}
+
+.modal__message__title {
+  @apply text-3xl text-white text-center uppercase;
+}
+
+.modal__message__description {
+  @apply text-xl text-white text-center;
+}
+
+.modal__message__description span {
+  @apply uppercase text-yellow-600 font-semibold;
+}
+
+.modal__buttons {
+  @apply flex justify-evenly w-full;
+}
+
+.modal__buttons__continue {
   @apply w-5/12 py-1 text-base px-2 uppercase border-2 border-green-300 text-green-300 rounded-lg;
 }
 
-.modal__options {
-  @apply w-5/12 py-1 text-base px-2 uppercase border-2 border-white text-white rounded-lg;
+.modal__buttons__options {
+  @apply w-full py-1 text-base px-2 uppercase border-2 border-white text-white text-center rounded-lg;
 }
 </style>
